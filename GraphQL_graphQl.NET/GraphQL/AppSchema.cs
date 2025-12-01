28427df7beb2c7dcdb7a54683f4dc50a24dff5d9
TanjilurNabil <86222610+TanjilurNabil@users.noreply.github.com>
2025-12-01 13:30:46 +0600

    package installed and test query (#1)
    Co-authored-by: tanjilur.nabil <tanjilur.nabil@savethechildren.org>

---
+++ GraphQL_graphQl.NET/GraphQL/AppSchema.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+using GraphQL_graphQl.NET.GraphQL.Queries;
+
+namespace GraphQL_graphQl.NET.GraphQL
+{
+    public class AppSchema:Schema
+    {
+        public AppSchema(IServiceProvider provider):base(provider)
+        {
+            Query = provider.GetRequiredService<AppQuery>();
+            //Mutation = provider.GetRequiredService<AppMutation>();
+        }
+    }
+}
